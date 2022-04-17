fe8a84ad7afae2ec8e52edb19e9da243d0745f0e
arpitkb <bansal_851937@student.nitw.ac.in>
2022-04-17 08:02:35 +0530

    Pulled

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,12 @@ import Header from "./components/Layout/Header";
 import SignupScreen from "./Screens/SignupScreen";
 import Privateroute from "./components/Routes/Privateroute";
 import AddProjectScreen from "./Screens/AddProjectScreen";
-<<<<<<< HEAD
 import VerifyUserScreen from "./Screens/VerifyUserScreen";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loadUser } from "./redux/action/auth";
-=======
 import Home from "./Screens/Home";
 import AllProjects from "./Screens/AllProjects";
->>>>>>> f7d706f6839f5769a2377752684dd143ee2e3809
 
 function App() {
   const dispatch = useDispatch();
@@ -31,12 +28,12 @@ function App() {
       <Container>
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/allProjects" element={<AllProjects />} />
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/signup" element={<SignupScreen />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/allProjects' element={<AllProjects />} />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/signup' element={<SignupScreen />} />
             <Route
-              path="/new-project"
+              path='/new-project'
               element={<Privateroute component={<AddProjectScreen />} />}
             />
             <Route
