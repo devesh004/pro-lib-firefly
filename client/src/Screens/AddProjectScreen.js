d087612e30e224e829d4a2368ad980f4995beaa6
arpitkb <bansal_851937@student.nitw.ac.in>
2022-04-17 05:39:37 +0530

    project add route

--- client/src/Screens/AddProjectScreen.js
+++ client/src/Screens/AddProjectScreen.js
@@ -7,6 +7,7 @@ import {
   Spinner,
   Alert,
   FloatingLabel,
+  FormControl,
 } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { colleges } from "../utils/CollgeNameList";
@@ -21,12 +22,18 @@ const AddProjectScreen = () => {
   const navigate = useNavigate();
   const { user, loading } = useSelector((state) => state.auth);
 
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(3);
   const [touched, setTouched] = useState(false);
   const [college, setCollege] = useState("");
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
+  const [domain, setDomain] = useState("");
+  const [description, setDesc] = useState("");
+  const [githubRepo, setGithubRepo] = useState("");
   const [err, setErr] = useState(null);
+  const [emailLoading, setEmailLoading] = useState(false);
+  const [mailData, setMailData] = useState(null);
+  const [otp, setOtp] = useState("");
   const mailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   const formSubmissionHandler = (e) => {
@@ -41,6 +48,8 @@ const AddProjectScreen = () => {
   const verifyEmail = async () => {
     console.log(college, email);
     try {
+      setEmailLoading(true);
+      setErr(null);
       const options = {
         headers: {
           "Content-Type": "application/json",
@@ -51,28 +60,59 @@ const AddProjectScreen = () => {
         { email },
         options
       );
+      setEmailLoading(false);
       console.log(data);
+      setMailData(data);
       setPage(2);
     } catch (err) {
-      setErr(err.message);
+      setErr("Email not sent");
+      setEmailLoading(false);
     }
   };
 
   const verifyOTP = () => {
-    console.log(college, email);
-    setPage(3);
+    setErr(null);
+    if (!Number(otp)) {
+      setErr("Please enter a valid otp");
+    } else if (mailData == null) {
+      console.log(mailData);
+      setErr("Email was not sent");
+    } else if (
+      Date.now() - mailData.created_at > 5 * 60 * 1000 ||
+      mailData.otp !== Number(otp)
+    ) {
+      setErr("OTP is not valid, please send again");
+      return;
+    } else {
+      setMailData(null);
+      setOtp("");
+      setEmail("");
+      setPage(3);
+    }
+  };
+
+  const addProject = () => {
+    if (
+      name.trim() === "" ||
+      domain === "" ||
+      githubRepo.trim() === "" ||
+      description.trim().length < 300 ||
+      description.trim().length > 500
+    )
+      return;
+    console.log(name, domain, githubRepo, description);
   };
 
   return (
     <>
       {/* <Meta title='Shopzone | Login' /> */}
       <Row className='justify-content-md-center'>
-        <Col md={6}>
+        <Col md={8}>
           <div className='display-6 mb-5'>Add your project</div>
 
           <div className='pt-3 pb-1'>
             {err && <Alert variant='danger'>{err}</Alert>}
-            {loading && (
+            {emailLoading && (
               <div>
                 <Spinner size='sm' animation='grow' variant='primary' />
                 <Spinner
@@ -82,7 +122,7 @@ const AddProjectScreen = () => {
                   variant='warning'
                 />
                 <Spinner size='sm' animation='grow' variant='success' />
-                <span className='mx-1 text-warning'>Loging you in</span>
+                <span className='mx-2 text-warning'>Sending mail</span>
               </div>
             )}
 
@@ -142,26 +182,19 @@ const AddProjectScreen = () => {
                 <FloatingLabel
                   className='mb-2'
                   label='Enter here'
-                  controlId='name'
+                  controlId='otp'
                 >
                   <Form.Control
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    name='name'
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
                     type='text'
-                    placeholder='Enter name'
+                    placeholder='Enter OTP'
                   />
-                  {touched && name.trim().length === 0 && (
-                    <Form.Text className='error-text'>
-                      Please enter project name
-                    </Form.Text>
-                  )}
                 </FloatingLabel>
                 <Button
                   className='btn-s'
                   style={{ width: "175px" }}
                   variant='success'
-                  type='submit'
                   onClick={verifyOTP}
                 >
                   Verify email
@@ -188,6 +221,7 @@ const AddProjectScreen = () => {
 
             {page === 3 && (
               <>
+                <div className='h5'>Add project details</div>
                 <FloatingLabel
                   className='mb-2'
                   label='Project name'
@@ -206,11 +240,67 @@ const AddProjectScreen = () => {
                     </Form.Text>
                   )}
                 </FloatingLabel>
+                <FloatingLabel
+                  className='mb-2'
+                  label='Select domain'
+                  controlId='domain'
+                >
+                  <Form.Select
+                    value={domain}
+                    onChange={(e) => setDomain(e.target.value)}
+                    aria-label='domain'
+                  >
+                    {domains.map((el, index) => (
+                      <option key={index} value={el}>
+                        {el}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </FloatingLabel>
+                <FloatingLabel
+                  className='mb-2'
+                  label='Github repo of project'
+                  controlId='github'
+                >
+                  <Form.Control
+                    value={githubRepo}
+                    onChange={(e) => setGithubRepo(e.target.value)}
+                    name='name'
+                    type='text'
+                    placeholder='Enter Github repo'
+                  />
+                  {touched && githubRepo.trim().length === 0 && (
+                    <Form.Text className='error-text'>
+                      Please enter a valid github repo
+                    </Form.Text>
+                  )}
+                </FloatingLabel>
+                <FloatingLabel
+                  className='mb-2'
+                  label='Brief description (min 300, max 500 chars)'
+                  controlId='github'
+                >
+                  <FormControl
+                    value={description}
+                    onChange={(e) => setDesc(e.target.value)}
+                    as='textarea'
+                    style={{ height: "200px" }}
+                    aria-label='description'
+                    placeholder='desc'
+                  />
+                  {touched &&
+                    (description.trim().length <= 300 ||
+                      description.trim().length > 500) && (
+                      <Form.Text className='error-text'>
+                        Please enter a valid github repo
+                      </Form.Text>
+                    )}
+                </FloatingLabel>
                 <Button
                   className='btn-s'
                   style={{ width: "175px" }}
                   variant='success'
-                  type='submit'
+                  onClick={addProject}
                 >
                   Add
                 </Button>
